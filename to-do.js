a07771c2167f4d3841eecc596487d7dce26d34a3
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-04-18 22:15:36 +0200

    updated/js
    changed format

--- to-do.js
+++ to-do.js
@@ -1,44 +1,4 @@
 
-function newItem(){
-
-       let li = document.createElement("li");
-       let inputValue = document.getElementById("input").value;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
-    
-       if (inputValue === '') {
-         alert("You must write something!");
-       } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
-       }
-    
-       function crossOut() {
-             li.classList.toggle("strike");
-         }
-    
-         li.addEventListener("dblclick",crossOut);
-    
-       let crossOutButton = document.createElement("crossOutButton");
-         crossOutButton.appendChild(document.createTextNode("X"));
-         li.appendChild(crossOutButton);
-    
-         crossOutButton.addEventListener("click", deleteListItem);
- 
-       function deleteListItem(){
-             li.classList.add("delete")
-         }
-
-       $('#list').sortable();
-    
-    }
-    
-    
-    
-      /*
-    // jQuery Code
-    //1. Adding a new item to the list:
-    
       let li = $('<li></li>');
       let inputValue = $('#input').val();
       li.append(inputValue);
@@ -48,7 +8,6 @@ function newItem(){
       } else {
         $('#list').append(li);
       }
-    //2. Crossing an item out:
       function crossOut() {
             li.toggleClass("strike");
         }
@@ -56,17 +15,10 @@ function newItem(){
         li.on("dblclick", function crossOut() {
             li.toggleClass("strike");
         });
-    //3. Adding a delete button
       let crossOutButton = $('<crossOutButton></crossOutButton>');
       crossOutButton.append(document.createTextNode('X'));
       li.append(crossOutButton);
-    
-    //   crossOutButton.on("click", deleteListItem);
-    //   function deleteListItem(){
-    // 		li.addClass("delete")
-    // 	}
        $('#list').sortable();
-    */
     
     
     
